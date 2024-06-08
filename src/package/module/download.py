6197392f17dd1b8f45806c9c58824f42322a85a3
= <=>
2024-06-08 12:49:32 +0200

    logging configuration for download tool

--- src/package/module/download.py
+++ src/package/module/download.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from requests import Response
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
@@ -8,11 +9,34 @@ from typing import Dict, List, Tuple, Union, Optional, Callable, ClassVar
 from dataclasses import dataclass, field
 from pydantic import BaseModel, Field
 import logging
+import logging.config
+import logging.handlers
 
 # Load environment variables from .env file
 load_dotenv()
 
+# BOE DOWNLOAD DATA
+BOE_WEB_URL = 'https://boe.es'
+BOE_SAVE_PATH = os.path.abspath("../../../data")
+
+# Logging configuration
+logger = logging.getLogger("Download_web_files_module")  # Child logger [for this module]
+# LOG_FILE = os.path.join(os.path.abspath("../../../logs/download"), "download.log")  # If not using json config
+
+
+def setup_logging() -> None:
+    """
+    Function to get root parent configuration logger.
+    Child logger will pass info, debugs... log objects to parent's root logger handlers
+    """
+    CONFIG_LOGGER_FILE = os.path.join(os.path.abspath("../../../config/loggers"), "download.json")
+    with open(CONFIG_LOGGER_FILE) as f:
+        content = json.load(f)
+    logging.config.dictConfig(content)
+
+
 class WebDownloadData(BaseModel):
+    """Web Download data object"""
     web_url: str
     local_dir: str
     fecha_desde: str
@@ -28,38 +52,45 @@ class Downloader:
 
     def download(self):
         destino_local = os.path.join(self.information.local_dir, 'boe', 'dias')
-        print(f"Destino local : {destino_local}")
+        logger.info(f"Destino local : {destino_local}")
+
         boe_api_sumario = f'{self.information.web_url}/diario_boe/xml.php?id=BOE-S-'
 
         # Fechas de inicio y fin para la descarga de documentos
         fecha_ini = datetime.strptime(self.information.fecha_desde, '%Y-%m-%d')  # format necesario : '2024-04-15'
         fecha_fin = datetime.strptime(self.information.fecha_hasta, '%Y-%m-%d')
-        print(f"Fecha inicio descarga : {fecha_ini}")
-        print(f"Fecha fin descarga: {fecha_fin}")
+        logger.info(f"Fecha inicio descarga : {fecha_ini}")
+        logger.info(f"Fecha fin descarga: {fecha_fin}")
 
         while fecha_ini <= fecha_fin:
             fecha_ymd = fecha_ini.strftime('%Y%m%d')  # formato cambiado a '20240415'
-            carpeta_fecha = os.path.join(destino_local, fecha_ini.strftime('%Y'), fecha_ini.strftime('%m'), fecha_ini.strftime('%d'))
+            carpeta_fecha = os.path.join(
+                destino_local, fecha_ini.strftime('%Y'), fecha_ini.strftime('%m'), fecha_ini.strftime('%d')
+            )
+
             fichero_sumario_xml = os.path.join(carpeta_fecha, 'index.xml')
-            
-            print(f"Ruta fichero sumario xml : {fichero_sumario_xml}")
+            logger.debug(f"Ruta fichero sumario xml : {fichero_sumario_xml}")
+
             # Eliminar el sumario XML si existe
             if os.path.exists(fichero_sumario_xml):
                 os.remove(fichero_sumario_xml)
-            
-            print(f'Solicitando {boe_api_sumario}{fecha_ymd} --> {fichero_sumario_xml}')
+
+            logger.info(f'Solicitando {boe_api_sumario}{fecha_ymd} --> {fichero_sumario_xml}')
+
             xml_content = self.get_xml_file(url=boe_api_sumario + fecha_ymd)
             self.save(content=xml_content, path=fichero_sumario_xml)
-            
+
             self.information.dw_files_paths = self.get_pdf_url_from_xml(archivo_xml=fichero_sumario_xml)
-            print(f'Numero de URLs de PDFs obtenidos para la fecha {fecha_ini} : {len(self.information.dw_files_paths)}')
-            
-            print(f'Guardando en destino documentos PDFs')
+            logger.info(
+                f'Numero de URLs de PDFs obtenidos para la fecha {fecha_ini} : {len(self.information.dw_files_paths)}'
+            )
+
+            logger.info('Guardando en destino documentos PDFs')
             for file_i, file_path in enumerate(self.information.dw_files_paths):
                 if (file_i + 1) <= self.information.batch:
                     file_name, response = self.get_files(
-                        web_url=self.information.web_url, 
-                        file_url=file_path, 
+                        web_url=self.information.web_url,
+                        file_url=file_path,
                         local_file_path=carpeta_fecha
                     )
                     self.save(content=response.content, path=os.path.join(carpeta_fecha, file_name))
@@ -79,7 +110,7 @@ class Downloader:
         if response.status_code == 200:
             return response.content
         else:
-            print(f'Error al descargar el documento: {response.status_code} URL: {url}')
+            logger.error(f'Error al descargar el documento: {response.status_code} URL: {url}')
         return None
 
     def get_pdf_url_from_xml(self, archivo_xml):
@@ -87,8 +118,8 @@ class Downloader:
         root = tree.getroot()
         # Extraemos todas las URLs de los PDFs
         #####
-        ## Podria añadir extraccion de metadatos del archivo xml para cada dia (un xml por dia y varios pdfs)
-        ## despues asociar esos metadatos a cada embedding de cada pdf de cada dia en el proceso de vewctorDB
+        # Podria añadir extraccion de metadatos del archivo xml para cada dia (un xml por dia y varios pdfs)
+        # despues asociar esos metadatos a cada embedding de cada pdf de cada dia en el proceso de vewctorDB
         #####
         urls_pdf = []
         for urlPdf in root.findall('.//urlPdf'):
@@ -109,12 +140,15 @@ class Downloader:
         """
         url_completa = web_url + file_url
         respuesta = requests.get(url_completa)
+
         if respuesta.status_code == 200:
             nombre_pdf = file_url.split('/')[-1]  # Extraemos el nombre del archivo desde la URL
-            print(f'Archivo descargado con éxito: {os.path.join(local_file_path, nombre_pdf)}')
-            return nombre_pdf, respuesta 
+            logger.info(f'Archivo descargado con exito: {os.path.join(local_file_path, nombre_pdf)}')
+
+            return nombre_pdf, respuesta
+
         else:
-            print(f'Error al descargar {url_completa}: {respuesta.status_code}')
+            logger.error(f'Error al descargar {url_completa}: {respuesta.status_code}')
             return "", respuesta
 
     def save(self, content, path):
@@ -124,19 +158,27 @@ class Downloader:
         # create file and write content
         with open(path, 'wb') as file:
             file.write(content)
-                
 
-if __name__ == "__main__":
+
+def main() -> None:
+    # set up the root logger configuration
+    setup_logging()
+
+    # set the child logger level
+    logger.setLevel(logging.INFO)
+
     data = WebDownloadData(
-                            web_url='https://boe.es',
-                            local_dir='C:\\Users\\Jorge\\Desktop\\MASTER_IA\\TFM\\proyecto\\data',
-                            fecha_desde='2024-04-15',
-                            fecha_hasta='2024-04-15',
-                            batch=10
-                        )
-    print(data.model_dump())
+        web_url=BOE_WEB_URL,
+        local_dir=BOE_SAVE_PATH,
+        fecha_desde='2024-04-15',
+        fecha_hasta='2024-04-15',
+        batch=20
+    )
+
+    logger.info(f"Download information: {data.model_dump()}")
     downloader = Downloader(information=data)
     downloader.download()
-    print(data.model_dump())
-    
-    
+
+
+if __name__ == "__main__":
+    main()
