ead07b4d9cf0a1990c19680bb57b2b4500efcde7
= <=>
2024-06-19 17:17:52 +0200

    vdb retrievers

--- src/VectorDB/db.py
+++ src/VectorDB/db.py
@@ -1,186 +1,74 @@
 from langchain_community.vectorstores import Chroma
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_core.embeddings import Embeddings
 from langchain_pinecone import PineconeVectorStore
 import chromadb
-from src.package.module.llm import EMBEDDING_MODEL,EMBEDDING_MODEL_GPT4
-from typing import List,Tuple
-
-INDEX_NAME = "llama3"
-
-## RETRIEVER FUNCTION 
-def docs_from_retriver(question :str, retriever):
-    try: 
-        return retriever.invoke(question)
-    except Exception as e:
-        print(f"{e}")
-
-    try: 
-        return retriever.invoke(question)
-    except Exception as e:
-        print(f"{e}")
+import logging 
+from dataclasses import dataclass
+from typing import Union, ClassVar
+from GRAPH_RAG.models import get_openai_emb, get_hg_emb
+from pydantic import BaseModel
+import test_db
 
 
+INDEX_NAME = "INDEX_DEFAULT_VDB_NAME"
 
-### Wrapperv to try and get info for db
-def try_db(func : callable = None):
+logger = logging.getLogger(__name__)
+
+
+@test_db.try_client_conexion(tries = 3)
+@test_db.try_retriever(try_query="¿que dia es hoy?")
+def get_chromadb_retriever(
+                            index_name :str = INDEX_NAME, 
+                            embedding_model : Embeddings = get_hg_emb, 
+                            collection_metadata : dict[str,str] = {"hnsw:space": "cosine"},
+                            search_kwargs : dict = {"k" : 3}
+                            ) -> tuple[VectorStoreRetriever,]:
     
-    def wrapper(*args, **kwargs):
-        
-        ## Verify the storage inside chroma database
-        chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone = func(*args, **kwargs)
-        
-        num = 2
-        try: 
-            for id in chroma_vectorstore.get()["ids"]:
-                if num > 0:
-                    print(chroma_vectorstore.get(include=['embeddings', 'documents', 'metadatas']))
-                    num -= 1
-                    
-            # Prueba sobre db usando el retriever
-            query = "La duración total de las enseñanzas en ciclos de grado medio"
-            response = retriever_chroma.invoke(query)
-            print("Number of embeddings retrieved : ", len(response))
-            try:
-                print("Best cosine similarity : ", response[0].page_content)
-            except Exception as e:
-                print(f"{e}")
-
-        except NameError as e:
-            print(f"{e}")
-
-
-        try:
-            # nota : Instanciar clase Chroma crea un objeto equivalnete a chroma_client de la libreria chromadb pero usando libreria langchain  
-            print("Collection info : ", chroma_vectorstore.get().keys())
-            print("Collection info ids len : ", (chroma_vectorstore.get()["ids"]))
-            print("Collection docs : ", chroma_vectorstore.get()["documents"])
-            try:
-                print("Collection embeddings (1st comp of first embedding) : ", chroma_vectorstore.get(include = ['embeddings'])["embeddings"][0][0])
-                print("LEN OF COLLECTION EMBEDDINGS: ", len(chroma_vectorstore.get(include = ['embeddings'])["embeddings"][0]))
-            except Exception as e:
-                print(f"{e}")
-        except NameError as e:
-            print(f"{e}")
-        
-        # Prueba sobre pinecone db usando el retriever
-        try:
-            query = "La duración total de las enseñanzas en ciclos de grado medio"
-            response = retriever_pinecone.invoke(query)
-            print("Number of embeddings retrieved :", len(response))
-            print("Best cosine similarity :\n", response[0].page_content)
-        except Exception as e:
-            print(f"{e}")
-            
-        return chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone
-    return wrapper
-
-
-
-### Db conexion
-@try_db
-def db_conexion() -> Tuple:
-    try : 
-        # Conexion to ChromaDB running in container locally
-        chroma_client = chromadb.HttpClient(host='localhost', port=8000)
-    except ValueError as e:
-        print(f"Not posible to connect to CHROMA DB: \n\t//Exception message : {e}")
-
-    # Delete index if already exists
-    try : 
-        chroma_client.delete_collection(name=INDEX_NAME)
-        print(f"CHROMA DB collection with name : {INDEX_NAME} deleted")
-    except:
-        print(f"No CHROMA DB collection with name {INDEX_NAME}")
-
-    # Initialize a collection inside the vectorDB from documents chunks
-    
-    # CHROMA DB
-    try:
+    client = chromadb.HttpClient(host='localhost', port=8000)
+    try: 
         chroma_vectorstore = Chroma(
-                                    embedding_function = EMBEDDING_MODEL,   
-                                    client = chroma_client,
-                                    collection_name=INDEX_NAME,
-                                    collection_metadata = {"hnsw:space": "cosine"} # dict o [deafult] None donde le puedes pasar metadata igual que se hace en el metodo 
-                                                                # : chroma_client.create_collection en su argumento (que tambien es un dict)
-                                                                # : "metadata" --- ejemplo metadata={"hnsw:space": "l2"} l2 is default
-                                            )
-
-        # Croma Retriever
-        retriever_chroma = chroma_vectorstore.as_retriever(search_kwargs = {"k" : 3})
-        print("Conexion to CHROMA DB vectorestore correct: \n\t//CHROMA vectorstore created\n\t//CHROMA Retriever created")
-        print(chroma_client.get_collection(name=INDEX_NAME))
+                                embedding_function=embedding_model,   
+                                client=client,
+                                collection_name=index_name,
+                                collection_metadata=collection_metadata
+                                )
+    except Exception as e:
+        logger.error(f"Error while connecting to Chroma DB -> {e}")
         
-    except Exception as e :
-        print(f"LOCAL CHROMA DB does not respond: \n\t//Exception message : {e}")
+    retriever =  chroma_vectorstore.as_retriever(search_kwargs = search_kwargs)
+    
+    return retriever , chroma_vectorstore
 
-
-
-    # PINECONE DB
+    
+@test_db.try_client_conexion(tries = 3)
+@test_db.try_retriever(try_query="¿que dia es hoy?")
+def get_pinecone_retriever(
+                            index_name :str = INDEX_NAME, 
+                            embedding_model : Embeddings = get_hg_emb, 
+                            search_kwargs : dict = {"k" : 3}
+                            ) -> VectorStoreRetriever:
     try:
         pinecone_vectorstore = PineconeVectorStore.from_existing_index(
-                                                                    index_name = INDEX_NAME, 
-                                                                    embedding = EMBEDDING_MODEL
-                                                                )
-        retriever_pinecone = pinecone_vectorstore.as_retriever(search_kwargs = {"k" : 3})
-        print("Conexion to Pinecone DB vectorestore correct: \n\t//Pinecone vectorstore created\n\t//Pinecone Retriever created")
-    except Exception as e :
-        print(f"Pinecone DB does not respond: \n\t//Exception message : {e}")
+                                                                        index_name = index_name, 
+                                                                        embedding = embedding_model
+                                                                    )
+    except Exception as e:
+        logger.error(f"Error while connecting to PineCone DB -> {e}")
         
-        
-    try_db(chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone)
-        
-    return chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone
-
-
-def try_db(func : callable = None):
+    retriever = pinecone_vectorstore.as_retriever(search_kwargs = search_kwargs)
     
-    def wrapper(*args, **kwargs):
-        
-        ## Verify the storage inside chroma database
-        chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone = func(*args, **kwargs)
-        
-        num = 2
-        try: 
-            for id in chroma_vectorstore.get()["ids"]:
-                if num > 0:
-                    print(chroma_vectorstore.get(include=['embeddings', 'documents', 'metadatas']))
-                    num -= 1
-                    
-            # Prueba sobre db usando el retriever
-            query = "La duración total de las enseñanzas en ciclos de grado medio"
-            response = retriever_chroma.invoke(query)
-            print("Number of embeddings retrieved : ", len(response))
-            try:
-                print("Best cosine similarity : ", response[0].page_content)
-            except Exception as e:
-                print(f"{e}")
-
-        except NameError as e:
-            print(f"{e}")
+    return retriever , pinecone_vectorstore
 
 
-        try:
-            # nota : Instanciar clase Chroma crea un objeto equivalnete a chroma_client de la libreria chromadb pero usando libreria langchain  
-            print("Collection info : ", chroma_vectorstore.get().keys())
-            print("Collection info ids len : ", (chroma_vectorstore.get()["ids"]))
-            print("Collection docs : ", chroma_vectorstore.get()["documents"])
-            try:
-                print("Collection embeddings (1st comp of first embedding) : ", chroma_vectorstore.get(include = ['embeddings'])["embeddings"][0][0])
-                print("LEN OF COLLECTION EMBEDDINGS: ", len(chroma_vectorstore.get(include = ['embeddings'])["embeddings"][0]))
-            except Exception as e:
-                print(f"{e}")
-        except NameError as e:
-            print(f"{e}")
-        
-        # Prueba sobre pinecone db usando el retriever
-        try:
-            query = "La duración total de las enseñanzas en ciclos de grado medio"
-            response = retriever_pinecone.invoke(query)
-            print("Number of embeddings retrieved :", len(response))
-            print("Best cosine similarity :\n", response[0].page_content)
-        except Exception as e:
-            print(f"{e}")
-            
-        return chroma_vectorstore,retriever_chroma,pinecone_vectorstore,retriever_pinecone
-    return wrapper
+def get_qdrant_retriever(embedding_model):
+    """_summary_
 
-    
+    Args:
+        embedding_model (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return None
+
---
+++ src/VectorDB/test_db.py
@@ -0,0 +1,79 @@
+from langchain_community.vectorstores import Chroma
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_core.embeddings import Embeddings
+from langchain_pinecone import PineconeVectorStore
+import chromadb
+import logging 
+from dataclasses import dataclass
+from typing import Union, ClassVar
+from GRAPH_RAG.models import get_openai_emb
+
+
+logger = logging.getLogger(__name__)
+
+
+
+# WRAPPERS FOR TRYING CLIENT DB CONEXION AND CHEKING DB CONTENT 
+def try_client_conexion(func : Union[callable,None] = None , tries : int = 2):
+    """
+    Wrapper -> try conexion to client vector db
+
+    Args:
+        func (Union[callable,None], optional): _description_. Defaults to None.
+    """
+    def wrapper(*args, **kwargs):
+        
+        retriever , vectorstore = func(*args, **kwargs)
+        logger.info(f"Trying client db {func.__name__}")
+        
+        # First test
+        try: 
+            for t in range(tries):
+                for id in vectorstore.get()["ids"]:
+                    logger.info(f"Try {t} for id : {id} -> {vectorstore.get(include=['embeddings', 'documents', 'metadatas'])}")          
+        except Exception as e:
+            logger.error(f"Client db First test error using {func.__name__} -> {e}")
+
+        # Second test
+        try:
+            # nota : Instanciar clase Chroma crea un objeto equivalnete a chroma_client de la libreria chromadb pero usando libreria langchain  
+            logger.info("Db Collection info : ", vectorstore.get().keys())
+            logger.info("Db Collection info ids len : ", (vectorstore.get()["ids"]))
+            logger.info("Db Collection docs : ", vectorstore.get()["documents"])
+            try:
+                logger.info(f"Db Collection embeddings (1st comp of first embedding) :  {vectorstore.get(include = ['embeddings'])['embeddings'][0][0]}")
+                logger.info(f"Db len of collection embeddings: {len(vectorstore.get(include = ['embeddings'])['embeddings'][0])}")
+            except Exception as e:
+                logger.error(f"Client db Second test [embeddings] error using {func.__name__} -> {e}")
+        except Exception as e:
+            logger.error(f"Client db Second test [Collection] error using {func.__name__} -> {e}")
+            
+        return retriever , vectorstore
+    
+    return wrapper
+
+def try_retriever(func : Union[callable, None] = None , try_query = "La duración total de las enseñanzas en ciclos de grado medio"):
+    """
+    Try retriever
+
+    Args:
+        func (Union[callable, None], optional): _description_. Defaults to None.
+        tries (int, optional): _description_. Defaults to 2.
+        try_query (str, optional): _description_. Defaults to "La duración total de las enseñanzas en ciclos de grado medio".
+    """
+    def wrapper(*args, **kwargs):
+        
+        retriever , vectorestore = func(*args, **kwargs)
+        logger.info(f"Trying retriever {func.__name__}")
+        
+        try:
+            response = retriever.invoke(try_query)
+            logger.info(f"Number of embeddings retrieved : {len(response)}")
+            if len(response) > 0:
+                logger.info(f"Best similarity retriever search : {response[0].page_content}")
+        except Exception as e:
+            logger.error(f"Retriever error using {func.__name__} -> {e}")
+            
+        return retriever , vectorestore
+    
+    return wrapper
