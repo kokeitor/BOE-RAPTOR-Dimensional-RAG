22b775f3d8f4f5e78400526cae43b1af1f5b0f7e
= <=>
2024-09-06 10:21:47 +0200

    finals results

--- src/main_rag_eval.py
+++ src/main_rag_eval.py
@@ -24,7 +24,7 @@ def main() -> None:
     # Create Raptor data (make cluster summary, process and store in vector database)
     ragas_evaluation = RagasEval(
         hg_token=str(os.getenv('HG_API_KEY')),
-        dataset_name="20240902203907"
+        dataset_name="20240903180604"
         )
     ragas_evaluation.run(results_file_path="./data/rag evaluation/results/results.csv", get_visual_reports=True)
     
